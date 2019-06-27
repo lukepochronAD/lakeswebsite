78861d6ecc853d4891fe7ff8fb9950847e31e2b2
Luke Pochron <luke.pochron@answerdigital.com>
2019-06-27 19:11:08 +0100

    finished, needs to be refactored

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,16 +1,22 @@
 let allPictures = document.getElementsByClassName("carousel");
 let currentIndex = 0;
 
+let toggleVisibility = () => {
+    document.getElementById("stopbutton").classList.toggle("is-hidden");
+    console.log("one done")
+    document.getElementById("playbutton").classList.toggle("is-hidden");
+    console.log("and the other");    
+};
+
 // prevent tall pictures from stretching the container.
 let height = document.getElementsByClassName("carousel")[0].height;
 var style = document.createElement('style');
 document.head.appendChild(style);
-style.sheet.insertRule(`.carousel {max-height: ${height}px}`);
+style.sheet.insertRule(`.carousel {max-height: ${height}px;}`);
 
 let next = () => {
    
         allPictures[currentIndex].classList.toggle('is-hidden');
-
 
         if(++currentIndex == allPictures.length){
             currentIndex = 0; // back to square one        
@@ -20,6 +26,35 @@ let next = () => {
 
 }
 
-setInterval(function(){
+let previous = () => {
+   
+    allPictures[currentIndex].classList.toggle('is-hidden');
+
+    console.log("hidden" + currentIndex)
+
+    if(--currentIndex == -1){
+        currentIndex = allPictures.length -1; // back to square one        
+    }
+
+    console.log("showing" + currentIndex)
+    allPictures[currentIndex].classList.toggle('is-hidden');
+
+}
+
+let timer;
+
+let startCarousel = () => {
+    toggleVisibility();
+    timer = setInterval(function(){
     next();
-}, 2500)
+}, 1600)};
+
+let stopCarousel = () => {
+    toggleVisibility();
+    clearInterval(timer);
+}
+
+document.getElementById("fwdbutton").addEventListener("click", next);
+document.getElementById("bwdbutton").addEventListener("click", previous);
+document.getElementById("playbutton").addEventListener("click", startCarousel);
+document.getElementById("stopbutton").addEventListener("click", stopCarousel);
